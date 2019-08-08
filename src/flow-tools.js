6f3d875cfd2bdfacee1ffd8c6c3a955d875f6143
Nathan Crowther <njcrowther@gmail.com>
2019-08-08 10:34:38 -0700

    initial version ready

--- src/flow-tools.js
+++ src/flow-tools.js
@@ -10,78 +10,38 @@ export default function() {
 
   selection.forEach((selected) => {
     var artboardName = selected.name
-    var artboardWidth = selected.width
+    var artboardWidth = selected.frame.width
 
-    // sketch.UI.message("Program gets here")
 
     // Create new text layer: here's where I make it fixedWidth and bottom aligned
-    try {
-      var newText = new Text({
-        parent: context.document.currentPage(),
-        text: artboardName,
-        name: artboardName,
-        // fixedWidth: true,
-        style:{
-          alignment: "left",
-          // verticalAlignment: 3,
-          textColor: '#000000'
-        },
-        frame: {width: selected.frame.width, height: 100}
-      });
-    }
-    catch (error) {
-      console.error(error)
-    }
+    var newText = new Text({
+      parent: context.document.currentPage(),
+      text: artboardName,
+      name: artboardName,
+      style:{
+        fontFamily: "Bogle",
+        fontWeight: "Regular",
+        fontSize: "14",
+        alignment: "left",
+        textColor: '#000000'
+      },
+      frame: {width: artboardWidth, height: 100}
+    });
 
+    // Set Fixed height/width (setTextBehaviour(2)) and bottom alignment
     newText.sketchObject.setTextBehaviour(2)
-    newText.sketchObject.verticalAlignment = 3
+    newText.style.verticalAlignment = Text.VerticalAlignment.bottom
 
-
-    // // Set Width and position of text layer, and align it with artboard
-    // newText.frame.width = selected.frame.width
-    // newText.frame.height = 100
-    //
-    // newText.style.verticalAlignment = 1
-    // newText.fixedWidth = true
-    // newText.frame.height = 100
-    // newText.style.verticalAlignment = 3
-
-
-
+    // Left align text box with artboard
     selected.selected = true
     newText.selected = true
 
-    actionWithType(context, "MSAlignLayersLeftAction").doPerformAction(nil);
-
+    // Vertical position of the text layer with the artboard
     newText.frame.y = selected.frame.y - 148
-
+    newText.frame.x = selected.frame.x
 
   })
 
   doc.sketchObject.inspectorController().reload()
 
 }
-
-
-function actionWithType(context,type) {
-	var controller = context.document.actionsController();
-
-	if (controller.actionWithName) {
-		return controller.actionWithName(type);
-	} else if (controller.actionWithID) {
-		return controller.actionWithID(type);
-	} else {
-		return controller.actionForID(type);
-	}
-}
-
-// See text style code:
-// console.log(JSON.stringify(selection))
-
-// console.log("Artboard's y is: " + selected.frame.y)
-// console.log("newText's y is: " + newText.frame.y)
-
-
-// console.log("Style is:" + JSON.stringify(newText.style))
-// console.log("FixedWidth is:" + newText.fixedWidth)
-// console.log("VerticalAlignment is: " + newText.style.verticalAlignment)
