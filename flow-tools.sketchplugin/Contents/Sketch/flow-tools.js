fa3b0c9605bc4aeb78d4de6df4975192209bd694
Nathan Crowther <njcrowther@gmail.com>
2019-09-19 16:52:57 -0500

    updated icon

--- flow-tools.sketchplugin/Contents/Sketch/flow-tools.js
+++ flow-tools.sketchplugin/Contents/Sketch/flow-tools.js
@@ -1,1 +1,181 @@
-var globalThis=this;function __skpm_run(e,t){globalThis.context=t;var n=function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}return n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,n,r){"use strict";r.r(n);var o=r(1).getSelectedDocument(),i=r(2).Text;n.default=function(){o.selectedLayers.forEach(function(e){var n=e.name,r=e.frame.width,o=new i({parent:t.document.currentPage(),text:n,name:n,style:{fontFamily:"Bogle",fontWeight:"Regular",fontSize:"14",alignment:"left",textColor:"#000000"},frame:{width:r,height:100}});o.sketchObject.setTextBehaviour(2),o.style.verticalAlignment=i.VerticalAlignment.bottom,e.selected=!0,o.selected=!0,o.frame.y=e.frame.y-116,o.frame.x=e.frame.x}),o.sketchObject.inspectorController().reload()}},function(e,t){e.exports=require("sketch")},function(e,t){e.exports=require("sketch/dom")}]);if("default"===e&&"function"==typeof n)n(t);else{if("function"!=typeof n[e])throw new Error('Missing export named "'+e+'". Your command should contain something like `export function " + key +"() {}`.');n[e](t)}}globalThis.onRun=__skpm_run.bind(this,"default");
+var globalThis = this;
+function __skpm_run (key, context) {
+  globalThis.context = context;
+
+var exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/flow-tools.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/flow-tools.js":
+/*!***************************!*\
+  !*** ./src/flow-tools.js ***!
+  \***************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+var sketch = __webpack_require__(/*! sketch */ "sketch"); // documentation: https://developer.sketchapp.com/reference/api/
+// var sketch = context.api()
+
+
+var doc = sketch.getSelectedDocument();
+
+var Text = __webpack_require__(/*! sketch/dom */ "sketch/dom").Text;
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  var selection = doc.selectedLayers;
+  selection.forEach(function (selected) {
+    var artboardName = selected.name;
+    var artboardWidth = selected.frame.width; // Create new text layer: here's where I make it fixedWidth and bottom aligned
+
+    var newText = new Text({
+      parent: context.document.currentPage(),
+      text: artboardName,
+      name: artboardName,
+      style: {
+        fontFamily: "Bogle",
+        fontWeight: "Regular",
+        fontSize: "14",
+        alignment: "left",
+        textColor: '#000000'
+      },
+      frame: {
+        width: artboardWidth,
+        height: 100
+      }
+    }); // Set Fixed height/width (setTextBehaviour(2)) and bottom alignment
+
+    newText.sketchObject.setTextBehaviour(2);
+    newText.style.verticalAlignment = Text.VerticalAlignment.bottom; // Left align text box with artboard
+
+    selected.selected = true;
+    newText.selected = true; // Vertical position of the text layer with the artboard
+
+    newText.frame.y = selected.frame.y - 116;
+    newText.frame.x = selected.frame.x;
+  });
+  doc.sketchObject.inspectorController().reload();
+});
+
+/***/ }),
+
+/***/ "sketch":
+/*!*************************!*\
+  !*** external "sketch" ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("sketch");
+
+/***/ }),
+
+/***/ "sketch/dom":
+/*!*****************************!*\
+  !*** external "sketch/dom" ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("sketch/dom");
+
+/***/ })
+
+/******/ });
+  if (key === 'default' && typeof exports === 'function') {
+    exports(context);
+  } else if (typeof exports[key] !== 'function') {
+    throw new Error('Missing export named "' + key + '". Your command should contain something like `export function " + key +"() {}`.');
+  } else {
+    exports[key](context);
+  }
+}
+globalThis['onRun'] = __skpm_run.bind(this, 'default')
+
+//# sourceMappingURL=flow-tools.js.map
